ba78fd040bb5ec6eac7565abc130946c78d38d47
admin165 <78504689@qq.com>
2021-08-05 19:30:29 +0800

    第一次修改

--- git-test-project/src/com/git/FristClass.java
+++ git-test-project/src/com/git/FristClass.java
@@ -4,4 +4,8 @@ public class FristClass {
     public static void main(String[] args){
         System.out.println("Hello world");
     }
+    public void test(){
+        int i = 0;
+        i += 10;
+    }
 }
